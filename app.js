8ffd0f3f2c2a1404a743fc0981db0913320cc52e
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-05-16 17:17:16 -0300

    layout

--- app.js
+++ app.js
@@ -8,19 +8,24 @@ function generateQRCode() {
   num = num.replace("-", "");
   num = num.replace(/\s/g, "");
 
-  let website = `https://wa.me/5522998465678?text=${name}%20,%20OI%20COMO%20da%20${msg}`;
+  let website = `https://wa.me/55${num}?text=${name}%20testando`;
   //let website = `https://api.whatsapp.com/send?phone=+5522998465678&text=%20oi`;
   //let salvarContato = `tel:${num}`;
   //window.location.replace("tel:22992427891");
   
-  window.location.href =
-    "https://wa.me/5522998465678?text=$%20,%20OI%20COMO%20da%20${msg}";
-  window.location = "tel:22992427891";
+  let primeiro = `https://wa.me/5522998465678?text=$%20,%20OI%20COMO%20da%20`;
+  let segundo = "tel:22992427891";
   
     let qrcodeContainer = document.getElementById("qrcode");
     qrcodeContainer.innerHTML = "";
     //Conectmessage();
-    new QRCode(qrcodeContainer, website);
+  new QRCode(qrcodeContainer, website);
+  setTimeout(() => {
+    
+  }, "10000");
+  setTimeout(() => {
+    window.location.replace(`tel:${num}`);
+  }, "10000");
 
     /*With some styles*/
 
