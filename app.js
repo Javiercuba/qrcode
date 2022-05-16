a2ccafdddb00328acaa9cc085e2400bbf2a76913
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-05-16 11:23:27 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+function generateQRCode() {
+  let num = document.getElementById("phone").value;
+  let msg = document.getElementById("msg").value;
+  let name = document.getElementById("name").value; //let website1 = document.getElementById("website").value;
+
+  num = num.replace('(', '');
+  num = num.replace(')', '');
+  num = num.replace("-", "");
+  num = num.replace(/\s/g, "");
+  
+
+  let website = `https://wa.me/55${num}?text=${name}%20,%20${msg}`;
+  if (website) {
+    let qrcodeContainer = document.getElementById("qrcode");
+    qrcodeContainer.innerHTML = "";
+    new QRCode(qrcodeContainer, website);
+    /*With some styles*/
+
+    document.getElementById("qrcode-container").style.display = "block";
+  } else {
+    alert("Please enter a valid URL");
+  }
+}
+function chengerInputCel(number) {
+  number = number.replace(/^(\d{2})/g, "(22) ");
+  number = number.replace(/^(\d{5})/g, "$1-");
+  number = number.replace(/(\d{5})(\d{4})/g, "$1-$2");
+
+  document.querySelector("input").value = number;
+  //document.querySelector("div").innerHTML = number;
+}
+
+
+
