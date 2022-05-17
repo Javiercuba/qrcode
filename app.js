78775c929f74595017f34c64890d6e1a6939337f
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-05-17 19:43:45 -0300

    Create server

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function generateQRCode() {
   num = num.replace(/\s/g, "");
   let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=Hello`;
 
-  let website = `https://wa.me/55${num}?text=${name}%20testando`;
+  let website = `https://wa.me/5521967808811?text=%20testando`;
   //let website = `https://api.whatsapp.com/send?phone=+5522998465678&text=%20oi`;
   let salvarContato = `tel:22992427891`;
   //window.location.replace("tel:22992427891");
@@ -21,6 +21,7 @@ function generateQRCode() {
   qrcodeContainer.innerHTML = "";
   //Conectmessage();
   new QRCode(qrcodeContainer, "https://javiercuba.github.io/qrcode/server.html");
+  
 
   /*With some styles*/
 
--- server.js
+++ server.js
@@ -28,7 +28,16 @@ function EnviarMsg() {
   window.location.replace(NumeroContato);
 }
 
+function Link() {
+  let website = `https://wa.me/5521967808811?text=%20testando`;
+
+  //let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=OIBB`;
+
+  window.location.replace(website);
+}
+
 function RealizarTarefas() {
-    SendMessage();
-    EnviarMsg();
+  //SendMessage();
+  Link();
+  EnviarMsg();
 }
