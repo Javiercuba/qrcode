076c41398535612c5c0f722351698a74a5d6ef78
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-05-17 18:47:15 -0300

    Create server

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function generateQRCode() {
   let qrcodeContainer = document.getElementById("qrcode");
   qrcodeContainer.innerHTML = "";
   //Conectmessage();
-  new QRCode(qrcodeContainer, 'server.js');
+  new QRCode(qrcodeContainer, 'data:server.html');
 
   /*With some styles*/
 
