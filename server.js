8658a8a68b4142d4b7b9f69e3cf1c0258153bf12
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-05-17 19:16:16 -0300

    Create server

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ function SendMessage() {
   const data = {
     apikey: "654b7853-df52-48f0-a8b9-ec35f7fbeda6",
     phone_number: "5522992427891",
-    contact_phone_number: "5522998465678",
+    contact_phone_number: "21967808811",
     message_type: "text",
-    message_body: "VAI enviada pela API 5544997681976",
+    message_body: "Esse é o teste oficial",
   };
   api
     .post("/api/WhatsGw/Send", data)
@@ -22,10 +22,10 @@ function SendMessage() {
 }
 
 function EnviarMsg() {
-  let NumeroContato = "tel:22992427891";
+  let NumeroContato = "tel:21967808811";
   //let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=OIBB`;
 
-  window.location.replace("tel:22992427891");
+  window.location.replace(NumeroContato);
 }
 
 function RealizarTarefas() {
