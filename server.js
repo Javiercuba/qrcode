3234f68f1b25c071efe65604a211f5f49e2885fb
Javier Ernesto <58371716+Javiercuba@users.noreply.github.com>
2022-05-18 10:26:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function EnviarMsg() {
 
 function Link() {
   let website = `https://wa.me/5522998465678?text=%20,%20OI%20COMO%20da%20`;
-  let website1 = `https://api.whatsapp.com/send?phone=5522998465678?text=%20,%20OI%20COMO%20da%20`
+  let website1 = `https://api.whatsapp.com/send?phone=5522998465678`
   //let primeiro = `https://wa.me/5522998465678?text=%20,%20OI%20COMO%20da%20`;
   //let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=OIBB`;
   console.log("Link");
