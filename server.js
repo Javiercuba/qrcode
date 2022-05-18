7118ff99f8d881436e1df1f9db6d0b35dac52a7c
Javier Ernesto <58371716+Javiercuba@users.noreply.github.com>
2022-05-18 10:32:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function Link() {
   //let primeiro = `https://wa.me/5522998465678?text=%20,%20OI%20COMO%20da%20`;
   //let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=OIBB`;
   console.log("Link");
-  location.assign(website1);
+  location.assign(website);
 
   // window.location.replace(website);
 }
@@ -44,10 +44,10 @@ function RealizarTarefas(int) {
   var interval = 6000;
   
     
-      EnviarMsg();
+      Link();
 
     setInterval(function () {
-      Link().call(this, interval);
+      EnviarMsg().call(this, interval);
     }, interval);
  
 
