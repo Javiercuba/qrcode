c028e0dc56a503de2544fbce94023646cb2a69f2
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-05-18 13:44:31 -0300

    Nova tela de salvar contato

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function SendMessage() {
     });
 }
 
-function EnviarMsg() {
+function SalvarContato() {
   let NumeroContato = "tel:21967808811";
   //let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=OIBB`;
   console.log("Numero Contato");
@@ -29,12 +29,12 @@ function EnviarMsg() {
   location.assign(NumeroContato);
 }
 
-function Link() {
+function EnviarMensagem() {
   let website = `https://wa.me/5522998465678?text=%20,%20OI%20COMO%20da%20`;
   let website1 = `https://api.whatsapp.com/send?phone=5522998465678`
   //let primeiro = `https://wa.me/5522998465678?text=%20,%20OI%20COMO%20da%20`;
   //let manda_msg = `https://app.whatsgw.com.br/api/WhatsGw/Send?apikey=654b7853-df52-48f0-a8b9-ec35f7fbeda6&phone_number=5522992427891&contact_phone_number=5522998465678&message_custom_id=mysoftwareid&message_type=text&message_to_group=0&message_body=OIBB`;
-  console.log("Link");
+  console.log("EnviarMensagem");
   location.assign(website);
 
   // window.location.replace(website);
@@ -47,9 +47,9 @@ function RealizarTarefas(int) {
       SendMessage();
 
     setInterval(function () {
-      EnviarMsg().call(this, interval);
+      SalvarContato().call(this, interval);
     }, interval);
  
 
-  // window.setInterval(EnviarMsg(), 4000);
+  // window.setInterval(SalvarContato(), 4000);
 }
